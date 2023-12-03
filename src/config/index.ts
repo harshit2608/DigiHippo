9e7bc9bbdb761a7d8b8e5039144e3dadd136c781
HARSHIT BARGUJAR <42277796+harshit2608@users.noreply.github.com>
2023-12-03 21:01:10 +0000

    Starting the Project of Digi Hippo

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+export const PRODUCT_CATEGORIES = [
+    {
+      label: "UI Kits",
+      value: "ui_kits" as const,
+      featured: [{
+        name: "Editor picks",
+        href: "#",
+        images: '/nav/ui-kits/mixed.jpg'
+      }]
+    }
+  ]
+  
